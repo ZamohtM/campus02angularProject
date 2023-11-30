1f5ac24f57941069c6dcda6d008b436657c0eddb
Markus Neuhauser <maki.neuhauser@gmail.com>
2023-11-30 21:08:46 +0100

    Bootstrap fix NEW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,6 +30,7 @@ import {BookLendingComponent} from "./components/bookLending/book-lending/book-l
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
